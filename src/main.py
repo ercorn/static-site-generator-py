a6596546c6679872c782aeefae6451e0c3c7bb9b
rcorn013 <rctexvn@gmail.com>
2025-01-28 09:58:54 -0500

    added conversion from markdown string to list of textnodes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from textnode import TextNode
 from htmlnode import *
+from inline_markdown import *
+import re
 
 def main():
     dummy_textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -18,6 +20,40 @@ def main():
     to_html_children_str = "".join(map(lambda node: node.to_html(), children_list))
     print(to_html_children_str)
     print(children_list[-1])
+
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    images = extract_markdown_images(text)
+    print(images)
+
+    text2 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    links = extract_markdown_links(text2)
+    print(links)
+
+    print("===TEST SPLIT LINK/IMAGE NODES===")
+    #for image in images:
+     #   image_alt, image_link = image[0], image[1]
+      #  print(f"alt: {image_alt} image link: {image_link}")
+       # sections = text.split(f"![{image_alt}]({image_link})", 1)
+        #print(sections)
+    split_link_node = re.split(r"(\[.*?\))", text)
+    print(split_link_node)
+
+    test_alt, test_link = extract_markdown_links(split_link_node[1])[0]
+    print(test_alt, test_link)
+
+    print("__________________________________________")
+    test_split_link_node = TextNode(
+    "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+    TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([test_split_link_node])
+    print(new_nodes)
+
+    print("___________++++++++++_________________")
+    final_text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    split_textnodes = text_to_textnodes(final_text)
+    print(split_textnodes)
+
 
 
 if __name__ == "__main__":
